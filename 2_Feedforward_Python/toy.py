aa60d6874f2d2b62f1411a47084a1f91edf9a385
Ruixin Chen <s210312@student.dtu.dk>
2021-09-12 22:01:30 +0800

    on_going

---
+++ 2_Feedforward_Python/toy.py
@@ -0,0 +1,60 @@
+# Copy and pasted from https://github.com/rasmusbergpalm/nanograd/blob/main/nanograd.py
+
+from math import exp, log
+
+class Var:
+    """
+    A variable which holds a float and enables gradient computations.
+    """
+
+    def __init__(self, val: float, grad_fn=lambda: []):
+        assert type(val) == float
+        self.v = val
+        self.grad_fn = grad_fn
+        self.grad = 0.0
+
+    def backprop(self, bp):
+        self.grad += bp
+        for input, grad in self.grad_fn():
+            input.backprop(grad * bp)
+
+    def backward(self):
+        self.backprop(1.0)
+
+    def __add__(self: 'Var', other: 'Var') -> 'Var':
+        return Var(self.v + other.v, lambda: [(self, 1.0), (other, 1.0)])
+
+    def __mul__(self: 'Var', other: 'Var') -> 'Var':
+        return Var(self.v * other.v, lambda: [(self, other.v), (other, self.v)])
+
+    def __pow__(self, power):
+        assert type(power) in {float, int}, "power must be float or int"
+        return Var(self.v ** power, lambda: [(self, power * self.v ** (power - 1))])
+
+    def __neg__(self: 'Var') -> 'Var':
+        return Var(-1.0) * self
+
+    def __sub__(self: 'Var', other: 'Var') -> 'Var':
+        return self + (-other)
+
+    def __truediv__(self: 'Var', other: 'Var') -> 'Var':
+        return self * other ** -1
+
+    def __repr__(self):
+        return "Var(v=%.4f, grad=%.4f)" % (self.v, self.grad)
+
+    def relu(self):
+        return Var(self.v if self.v > 0.0 else 0.0, lambda: [(self, 1.0 if self.v > 0.0 else 0.0)])
+
+if __name__ == "__main__":
+    a = Var(3.0)
+    b = Var(5.0)
+    c = a * b
+    d = Var(9.0)
+    e = a * d
+    f = c + e
+
+    f.backward()
+
+    for v in [a, b, c, d, e, f]:
+        print(v)
